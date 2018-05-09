a85c09eb56e90d4b419920cc8892cf240fbc7817
Mohammed Minhaz <mminhaz@ibm.com>
2018-05-09 17:48:14 -0400

    updates

---
+++ src/app/services/geoname.service.ts
@@ -0,0 +1,27 @@
+
+import { Injectable } from '@angular/core';
+import { Http, Response } from '@angular/http';
+import { Observable } from 'rxjs/Observable';
+
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/do';
+import { ICountry } from '../model/country.model';
+
+@Injectable()
+export class GeonameService {
+  private URL: string = "http://api.geonames.org/countryInfoJSON?username=mminhaz93";
+
+  constructor(private _http: Http) { }
+
+  getCountries(): Observable<ICountry[]> {
+    return this._http
+        .get(this.URL)
+        .map((response: Response) => <ICountry[]> response.json().geonames)
+        .catch(error => {
+                console.error(error);
+                return Observable.throw(error.json())
+        });
+    }
+}
