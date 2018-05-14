492c239856832c6d4fa955d48b333fe68cf52030
Minhaz <Minhaz@Minhaz-2.local>
2018-05-14 18:49:35 -0400

    search geonames

--- src/app/pipes/country.pipe.ts
+++ src/app/pipes/country.pipe.ts
@@ -2,11 +2,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({ name: 'country' })
 export class CountryPipe implements PipeTransform {
   transform(country: any, searchText: any): any {
-    if(searchText == null) return country;
+    if (searchText == null) return country;
 
-    return country.filter(function(category){
+    return country.filter(function (category) {
       return category.countryName.toLowerCase().indexOf(searchText.toLowerCase()) > -1;
     })
-  } 
+  }
 }
 
