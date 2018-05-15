e58ac7062fcf2bbd058fb16c588820cfba3d5c5c
Minhaz <Minhaz@Minhaz-2.local>
2018-05-15 01:20:28 -0400

    search checkbox

--- src/app/components/projects/geonames/geonames.component.ts
+++ src/app/components/projects/geonames/geonames.component.ts
@@ -16,7 +16,7 @@ export class GeonamesComponent implements OnInit {
   copyCountries: ICountry[] = [];
 
   constructor(private _geonameService: GeonameService) {
-    
+
   }
 
   ngOnInit() {
@@ -40,13 +40,19 @@ export class GeonamesComponent implements OnInit {
         //console.log('all countries clicked');
         break;
       case "europe":
-        this.countries = this.countries.filter(country => {
+        this.countries = this.copyCountries.filter(country => {
           return country.continentName.toLowerCase().includes("europe");
         });
         //console.log('show only european countries');
         break;
+      case "asia":
+        this.countries = this.copyCountries.filter(country => {
+          return country.continentName.toLowerCase().includes("asia");
+        });
+        //console.log('show only european countries');
+        break;
       case "pop":
-        this.countries = this.countries.filter(country => {
+        this.countries = this.copyCountries.filter(country => {
           return parseInt(country.population) > 1000000;
         });
         //console.log('show poulation > 1M');
@@ -59,7 +65,7 @@ export class GeonamesComponent implements OnInit {
     this.column = property;
     let direction = this.isDesc ? 1 : -1;
 
-    this.countries.sort(function(a, b) {
+    this.countries.sort(function (a, b) {
       if (a[property] < b[property]) {
         return -1 * direction;
       } else if (a[property] > b[property]) {
