35ce75b55defa382e46584d41840b22c8f1798d1
Mohammed Minhaz <mminhaz@ibm.com>
2018-05-23 04:01:49 -0400

    resume and blog

---
+++ src/app/services/blogs.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from "@angular/core";
+//Firebase
+import { AngularFireDatabase, AngularFireList } from "angularfire2/database";
+
+@Injectable()
+export class BlogsService {
+  blogsList: AngularFireList<any>;
+  constructor(private firedb: AngularFireDatabase) {}
+
+  // Get all list
+  getBlogs() {
+    this.blogsList = this.firedb.list("blogs");
+    return this.blogsList;
+  }
+
+  //add new todo
+  addBlog(blogDate, blogTitle, blogLink) {
+    this.blogsList.push({
+      date: blogDate,
+      title: blogTitle,
+      link: blogLink
+    });
+  }
+}
